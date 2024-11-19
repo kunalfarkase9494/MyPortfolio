b2e54bf8299c38284a8dc68a99433069c28e3f2f
Kunal Farkase <112757045+kunalfarkase9494@users.noreply.github.com>
2024-11-19 13:50:33 +0530

    Update Hero.jsx

--- src/hero/Hero.jsx
+++ src/hero/Hero.jsx
@@ -1,5 +1,7 @@
 import './Hero.css'
 import kunalImage from '../assets/Kunal1.jpg'
+import kunalResume from '../assets/Kunal_Resume.pdf'
+
 const Hero = () => {
   return (
     <>
@@ -20,7 +22,7 @@ const Hero = () => {
               efficient web applications.
             </p>
             <br />
-            <a href="src/assets/Kunal_Resume.pdf" download="Kunal's_Resume.pdf">
+            <a href={kunalResume} download="Kunal's_Resume.pdf">
               <button className="btn btn-primary">Download Resume</button>
             </a>
           </div>
