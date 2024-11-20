096111b682738cc34c4075c8f191773df31b9267
Kunal Farkase <112757045+kunalfarkase9494@users.noreply.github.com>
2024-11-20 20:10:01 +0530

    Update Contact.jsx

--- src/Contact/Contact.jsx
+++ src/Contact/Contact.jsx
@@ -1,27 +1,53 @@
-
-import React, { useEffect } from 'react'
+import React, { useState,useEffect } from 'react'
 import './Contact.css';
 
 function Contact() {
+
+    const [formData, setFormData] = useState({
+        name: '',
+        email: '',
+        msg: ''
+      });
+    
+      const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData({
+          [name]: value,
+        });
+      };
+    
+      const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log('Form submitted:', formData);
+    
+        // Clear the form fields
+        setFormData({
+          name: '',
+          email: '',
+          msg: ''
+        });
+    }
     
   return (
     <>
         <div className='contact-section' id='contact'>
-        <div className='container'>
-            <h1 className='text-center mb-4'>Get In Touch</h1>
+        <div className='container-fluid'>
+            <h1 className='text-center mb-6'>Get In Touch</h1>
         </div>
-        <div className='contact-form-container shadow-sm col-lg-8 mx-lg-auto bg-white p-2 p-lg-5'>
-            <form action="#" onSubmit={(e)=>{e.preventDefault()}} method='post'>
+        <div className='contact-form-container shadow-sm col-lg-8 mx-lg-auto bg-white p-3 p-lg-5'>
+            <form action="#" onSubmit={handleSubmit} method='post' >
             <div className='row '>    
                 <div class="col-md-6 col-sm-12 mb-3">
-                    <input type="text" name='name' className='form-control ' placeholder='Name'/>
+                    <input type="text" name='name' className='form-control '  onChange={handleChange} value={formData.name} placeholder='Name'/>
                 </div>
                 <div className='col-md-6 col-sm-12 mb-3'>
-                    <input type="email" name='email' className='form-control' placeholder='Email'/> 
+                    <input type="email" name='email' className='form-control'  onChange={handleChange} value={formData.email} placeholder='Email'/> 
                 </div>
             </div>
-            <textarea type="text" name='msg' className='form-control mb-3' rows='3' placeholder='Enter your message'/>
+            <div className='col-sm-12 mb-3'>
+            <textarea type="text" name='msg' className='form-control mb-3'  onChange={handleChange} value={formData.msg} rows='3' placeholder='Enter your message'/>
             <button type='submit' className='btn btn-primary form-control mb-2'>Send it</button> 
+            </div>
             </form>   
             <div className='container text-center'>
                 <p>or</p>
